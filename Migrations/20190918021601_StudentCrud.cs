b6b836c9589681bf952ae297c46100890b277bc1
Adesanya Abidemi <abidemiadesanya2016@gmail.com>
2019-09-17 20:57:43 -0700

    New Update

---
+++ Migrations/20190918021601_StudentCrud.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentRegCrudOperation.Migrations
+{
+    public partial class StudentCrud : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    Address = table.Column<string>(maxLength: 200, nullable: false),
+                    Email = table.Column<string>(maxLength: 50, nullable: false),
+                    MatriNo = table.Column<string>(maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegCrudOperation.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        //[Column(TypeName = "varchar")]
+        [DisplayName("FullName")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Name  must be between 3 and 200 characters")]
+        public string Name { get; set; }
+
+        //[Column(TypeName = "varchar")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Address is required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Address  must be between 3 and 200 characters")]
+        public string Address { get; set; }
+
+       // [Column(TypeName = "varchar")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        //[Column(TypeName = "varchar")]
+        [DisplayName("Matric No")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Matric Number is required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Matric Number  must be between 4 and 20 characters")]
+        public string MatriNo { get; set; }
+    }
+}
